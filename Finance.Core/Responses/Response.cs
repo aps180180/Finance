695210f51eea3908934c4516c766fae709007be9
aps180180 <aps180180@gmail.com>
2024-06-16 19:52:30 -0300

    adicionado a padronizacao

---
+++ Finance.Core/Responses/Response.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Finance.Core.Responses
+{
+    public class Response<TData>
+    {
+        private const int DefaultStatusCode = 200;
+        private readonly int _code;
+
+        //contrutor parameterless - 
+        [JsonConstructor]
+        public Response()
+        {
+            _code = DefaultStatusCode;
+            
+        }
+        public Response( TData? data, int code=DefaultStatusCode, string? message= null)
+        {
+            Data = data;
+            Message = message;
+            _code = code;
+        }
+        public TData? Data { get; set; }
+        public string? Message { get; set; }
+
+        [JsonIgnore]
+        public bool IsSuccess => _code is >= 200 and <= 299; 
+
+    }
+}
